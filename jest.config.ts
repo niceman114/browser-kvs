1e97e13ea4b7cb9e6d5d2c01b6d7601f85ab17ee
niceman114 <niceman114@gmail.com>
2025-01-06 10:55:11 +0900

    :wrench: chore: Update jest.config.ts to include `jest-junit` reporter

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,16 @@ export default {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: "<rootDir>"}),
   modulePaths: ["<rootDir>/src"],
   preset: "ts-jest",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./test-reports",
+        outputName: "junit.xml",
+      },
+    ],
+  ],
   roots: ["<rootDir>/tests"],
   testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
